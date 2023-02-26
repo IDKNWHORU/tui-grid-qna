b3f3d9751aad1868e21e7d83fc7e026db5b1a8d6
junhojava <junho9341@gmail.com>
2023-02-26 13:12:20 +0900

    add localStorage push data feature

--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,20 @@
 import { App } from "./App.js";
+import { PersonalInfoRepository } from "./repository/PersonalInfoRepository.js";
+
+const personalInfoRepository = new PersonalInfoRepository();
+if (localStorage.getItem("personalInfo") === null) {
+  const personalInfoList = await personalInfoRepository.callApi();
+  localStorage.setItem(
+    "personalInfo",
+    JSON.stringify(
+      personalInfoList.map((personalInfo, idx) => ({ idx, ...personalInfo }))
+    )
+  );
+}
+
+if (localStorage.getItem("cardStatus") === null) {
+  localStorage.setItem("cardStatus", "[]");
+}
 
 customElements.define("pr-app", App);
 const prApp = document.createElement("pr-app");
